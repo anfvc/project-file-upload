c67d2938757a700fa12e2ce3ecfdd780e5a3e43c
andres <andres.villay@dci-student.org>
2024-06-11 11:17:32 +0200

    first commit file-upload-project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,108 @@
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import Album from "./Album";
+
+function App() {
+  const [inputs, setInputs] = useState({});
+  const [albums, setAlbums] = useState([]);
+  const [album, setAlbum] = useState([]);
+
+  const fileInput = useRef(null); // 1. this will hold a reference to our file input!
+
+  useEffect(() => {
+    getAlbums();
+  }, [album]);
+
+  async function getAlbums() {
+    const response = await fetch("http://localhost:3002");
+    if (response.ok) {
+      const data = await response.json();
+      setAlbums(data);
+    }
+  }
+
+  function handleChange(e) {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", inputs.title);
+    formData.append("year", inputs.year);
+    formData.append("artist", inputs.artist);
+    formData.append("jacket", inputs.jacket);
+
+    setInputs({});
+    fileInput.current.value = ""; // 3. this resets the file input value :)
+
+    try {
+      const response = await fetch("http://localhost:3002/add", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setAlbum(data);
+        alert(`added!`);
+      } else throw new Error("something went wrong...");
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h1>My Favorites</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          placeholder="artist"
+          onChange={handleChange}
+          value={inputs.artist || ""}
+          name="artist"
+        />
+        <input
+          type="text"
+          placeholder="title"
+          onChange={handleChange}
+          value={inputs.title || ""}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="year"
+          onChange={handleChange}
+          value={inputs.year || ""}
+          name="year"
+        />
+        <input
+          type="file"
+          ref={fileInput} // 2. this sets the reference to file input
+          onChange={(e) => setInputs({ ...inputs, jacket: e.target.files[0] })}
+          accept="image/*"
+        />
+        <button>Add</button>
+      </form>
+      {!!albums.length &&
+        albums.map(album => (
+          <Album
+            key={album._id}
+            album={album}
+            getAlbums={getAlbums}
+            inputs={inputs}
+            setInputs={setInputs}
+            setAlbum={setAlbum}
+          />
+        ))}
+    </>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,84 @@
+import express from "express";
+import mongoose from "mongoose";
+import Album from "./Model/Album.js";
+import cors from "cors";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./frontend/public/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: { fileSize: 150000 }, // 150kb
+});
+
+const upload = multer({ storage: storage });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//* Store your own MongoDB connection string in .env file!
+try {
+  await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+  console.log("Database is connected! 🐱");
+} catch (error) {
+  console.log(error.message);
+  console.log("Database connection failed... :(");
+}
+
+//* Default route
+app.get("/", async (req, res) => {
+  const albums = await Album.find();
+  res.status(200).json(albums);
+});
+
+app.post("/add", upload.single("jacket"), async (req, res, next) => {
+  try {
+    let newAlbum = new Album(req.body);
+    newAlbum.jacket = req.file.filename;
+    await newAlbum.save();
+    res.status(200).json(newAlbum);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/delete/:id", async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const toDelete = await Album.findByIdAndDelete(id);
+    res.status(200).json(toDelete);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.patch("/update/:id", upload.single("jacket"), async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    let toUpdate = await Album.findByIdAndUpdate(id, {jacket: req.file.filename}, {new: true});
+
+    res.status(200).json(toUpdate);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//* Global Error Handling
+app.use(function errorHandler(err, req, res, next) {
+  console.log("error handler is running...");
+  res.status(err.status || 400).send({
+    error: {
+      message: err.message,
+      status: err.status,
+    },
+  });
+});
+
+app.listen(3002, () => {
+  console.log("Server is listening to port 3002");
+});
